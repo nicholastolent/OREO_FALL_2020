e210d25fdea4f09ad60ca4831781327db04144d8
nicholastolent <ntolentino2017@my.fit.edu>
2020-10-08 19:29:38 -0400

    Encoded sending Data from client

--- Socket_Connection/client.py
+++ Socket_Connection/client.py
@@ -1,5 +1,6 @@
 import socket
 import configparser as CP
+import os
 
 configParser = CP.RawConfigParser()
 configFilePath = 'config.conf'
@@ -8,9 +9,14 @@ configParser.read(configFilePath)
 HOST = configParser.get('Host.IP.Info', 'ADDRESS')
 PORT = int(configParser.get('Host.IP.Info', 'PORT'))
 
+fileName = "testMsg.txt"
+
+filesize = os.path.getsize(fileName)
+
+message = "Hello World"
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM)as s:
     s.connect((HOST,PORT))
-    s.sendall(b'Hello, World')
+    s.send(message.encode('utf8'))
     data = s.recv(1024)
     
-print('Received', repr(data))
+print('Received', repr(data.decode()))
